cd6989db4d342fbcecd712f7888bdbe08e7752fa
virusiq1 <92710610+virusiq1@users.noreply.github.com>
2022-12-23 08:54:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,16 +6,16 @@ _set_bot () {
     echo "جاري تنزيل اكواد سورس فايروس "
     wget -q https://github.com/virusiq1/VirusMusic/archive/main.zip -O "$zippath"
     echo " تفريغ البيانات "
-    CATPATH=$(zipinfo -1 "$zippath" | grep -v "/.");
     unzip -qq "$zippath"
     echo " تم التفريغ "
     echo " يتم التنظيف "
     rm -rf "$zippath"
-
+    sleep(5)
+    python3 setup/updater.py requirements.txt requirements.txt
+    sleep(5)
     cd "VirusMusic-main"
-    _run_cat_git
-    python3 ../setup/updater.py ../requirements.txt requirements.txt
-    chmod -R 755 bin
+
+    
     echo "    بدء تنزيل بيانات فايروس    "
     echo "
 
